63d657ef3e6a911ff04e6960878021e04469fc77
Elias Urrutia <elias-samp@hotmail.com>
2021-10-16 03:13:15 -0300

    xD
    GOOD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { 
+  HeaderComponent,
+  ArticlesComponent,
+  HomeComponent,
+  SharedModule
+} from './shared';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
